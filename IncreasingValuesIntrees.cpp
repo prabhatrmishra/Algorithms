6ccc6c1ad49781305329f0726e5087baf370deab
unknown <prabhatranjanm98@gmail.com>
2018-05-30 20:06:25 +0530

    Increasing Key Problem solved

--- IncreasingValuesIntrees.cpp
+++ IncreasingValuesIntrees.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 #include <algorithm>
 using namespace std;
-typedef long long int ll;
+typedef long long  ll;
 struct Node
 {
     ll data;
@@ -16,7 +16,7 @@ struct Node
 vector< struct Node *> nodeArray;
 vector<ll> sumArray;
 map<struct Node * , ll> mp;
-
+map<struct Node *, int>isVisited;
 struct Node* newnode()
 {
     struct Node* node = new Node;
@@ -40,15 +40,24 @@ ll sumSubtreeUtil(struct Node *ptr,  int sum)
         {
          //  cout<<"Leaves Node"<<ptr->data<<endl;
             cur_sum += ptr->data;
+            isVisited[ptr] = 1;
         }
     else{
-         cur_sum += (ptr->data) ;
-         for(ll i = 0; i < lst.size(); i++)
-         {
-              struct Node * child = lst[i];
-              cur_sum  +=  sumSubtreeUtil(child, sum );
-         }
-    }
+             if(isVisited[ptr] == 0)
+             {
+                   cur_sum += (ptr->data) ;
+                   isVisited[ptr] = 1;
+             }
+               
+             for(ll i = 0; i < lst.size(); i++)
+             {
+                  struct Node * child = lst[i];
+                  if(isVisited[child] == 0)
+                    cur_sum  +=  sumSubtreeUtil(child, sum );
+                //else
+                    //return 0;
+             }
+       }
     mp[ptr] =  cur_sum;
     return cur_sum;
 }
@@ -81,6 +90,13 @@ void  getSum(){
     }
     return;
 }
+void clearIsVisited(){
+    for(int i =0 ; i < nodeArray.size(); i ++)
+    {
+        struct Node * temp = nodeArray[i];
+        isVisited[temp]=  0;
+    }
+}
 // driver program to run the case
 int main()
 {
@@ -97,10 +113,10 @@ int main()
           cin>>u >> v;
           struct Node * u_node = nodeArray[u-1];
           struct Node * v_node = nodeArray[v-1];
-          vector<struct Node *> child_list  = u_node->child_list;
-          child_list.push_back(v_node);
-          u_node->child_list = child_list;
-          v_node->parent = u_node;
+         v_node->parent = u_node;
+        v_node->child_list.push_back(u_node);
+        u_node->child_list.push_back(v_node); 
+      //  u_node->parent = v_node;
 
     }
     for(ll i = 0; i < n; i++)
@@ -113,31 +129,38 @@ int main()
     
      //for(int i = 0; i < n; i++)
       //   cout<<nodeArray[i]->data<<","<<endl;
-        sumSubtree(nodeArray[0],x);
-  // getSum();
+       // sumSubtree(nodeArray[0],x);
+        // getSum();
         while(q--)
         {
+                clearIsVisited();
                 ll a,d, val = 0;
                 cin >>d>>a;
                 struct Node * curr;
                 curr = nodeArray[d-1];
-                mp[curr] += a; 
-                struct Node * parent_node = curr->parent;
-                while(parent_node != NULL)
+                curr ->data += a;
+               mp[curr] += a; 
+            /*    struct Node * parent_node = curr->parent;
+              while(parent_node != NULL)
                 {
                       struct Node *  _parent = parent_node;
-                      mp[parent_node] += a;
-                      parent_node = _parent ->parent;
-                }
-                // sumSubtree(nodeArray[0],x); 
+                      if(isVisited[parent_node] == false)
+                      {
+                            mp[parent_node] += a;
+                            parent_node = _parent ->parent;
+                      }
+                       
+                }*/
+                sumSubtree(nodeArray[0],x); 
                 for(ll i = 0; i < nodeArray.size(); i++ )
                 {   
                     curr = nodeArray[i];
-              //    cout<<"Sum at node "<< i+1 <<"  : "<<mp[curr]<<endl;
+            //    cout<<"Sum at node "<< i+1 <<"  : "<<mp[curr]<<endl;
                     if(mp[curr] > x)
                         val++;
                 }
                cout<<val<<endl;
+             
     }
-   
+   return 0;
 }
