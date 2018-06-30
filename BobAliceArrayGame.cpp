949f09b0a2310b8e75288d4f076b4c0321c43644
unknown <prabhatranjanm98@gmail.com>
2018-07-01 02:46:02 +0530

    Bob And Alice Game question solved

---
+++ BobAliceArrayGame.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef unsigned long long  ll; 
+typedef even 1;
+typedef odd 0;
+ll a[200000];
+int getMaxIndex(int start ,int end)
+{
+    if(a[start] >= a[end])
+        return start;
+    else
+        return end;
+}
+int main()
+{
+    ll N, val, alex_score, bob_score ;
+    int start, end;  
+    int flag , turn , oddSelect;
+    cin>>N;
+    for(int i = 0; i < N ; i++)
+    {
+        cin>> val;
+        a[i] = val;
+    }
+    
+    if((N % 2) == 1)
+    {
+        start = end = (N/2) +1;
+        turn = odd;
+    }
+    else
+    {
+        start = (N/2) -1;
+        end = (N/2);
+        turn = even;
+    }
+    flag = 1;
+    oddSelect = 0;
+    while((start >= 0) || (end < N) )
+    {
+        int val ;
+        if((turn == odd) && (oddSelect == 0)  )
+        {
+            val = a[start];
+            start --;
+        }
+         if((turn == odd) && (oddSelect == 1)  )
+        {
+            val = a[end];
+            end++;
+        }
+        if(turn == even)
+        {
+            int index = getMaxIndex(start , end);
+             val = a[index];
+            if(index == start)
+            {
+                start -- ;
+                oddSelect = 1;
+            }
+            if(index == end)
+            {
+                end ++;
+                oddSelect = 0;
+            }
+        }
+        if(flag == 0 )
+            alex_score += val;
+        else
+            bob_score += val;
+        flag = 1-flag;
+    }
+}
+    
+    return 0;
+}
